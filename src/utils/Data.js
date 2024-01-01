0e1555be8f3f4c07b054c504c00f005308551951
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-01-02 00:12:50 +0530

    Data Implemented

---
+++ src/utils/Data.js
@@ -0,0 +1,43 @@
+const Data = [
+    {
+        id: '1',
+        name: 'Chicken Burger',
+        ingredients: 'Chicken Burger',
+        price: '7.10',
+        rating:4,
+        image: require('../assets/chickenBurger.png'),
+    },
+
+    {
+        id: '2',
+        name: 'Cup Cake',
+        ingredients: 'Cup Cake',
+        price: '7.10',
+        rating:5,
+        image: require('../assets/cupcake.png'),
+    },
+    {
+        id: '3',
+        name: 'Fast-food',
+        ingredients: 'fast-food',
+        price: '5.10',
+        rating:3,
+        image: require('../assets/fast-food.png'),
+    },
+    {
+        id: '4',
+        name: 'French Fries',
+        ingredients: 'Salmon Meat',
+        price: '9.55',
+        rating:1,
+        image: require('../assets/french-fries.png'),
+    },
+    {
+        id: '5',
+        name: 'Food Delivery',
+        ingredients: 'Salmon Meat',
+        price: '9.55',
+        rating:0,
+        image: require('../assets/food-delivery.png'),
+    }
+]
---
+++ src/utils/categories.js
@@ -0,0 +1,9 @@
+const categories = [
+    {id: '1', name: 'pizza', image: require('../assets/catergories/pizza.png')},
+    {id: '2', name: 'Burger', image: require('../assets/catergories/burger.png')},
+    {id: '3', name: 'Fruits', image: require('../assets/catergories/fruits.png')},
+    {id: '4', name: 'Noodles', image: require('../assets/catergories/noodles.png')},
+  ];
+  
+  export default categories;
+  
